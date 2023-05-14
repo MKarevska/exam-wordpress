194035c1f37bb6d324315f2db2f83c254a54d86e
MKarevska <meri_kar@abv.bg>
2023-05-14 10:51:31 +0300

    Initial commit of the new theme shop for washing machine

---
+++ wp/wp-content/themes/shop/functions.php
@@ -0,0 +1,19 @@
+<?php
+function shop_style() {
+    wp_enqueue_style(
+        'shop',
+        get_template_directory_uri() . '/css/master.css', array(),
+        filemtime(  get_template_directory() . '/css/master.css' ) );
+}
+add_action( 'wp_enqueue_scripts', 'shop_style' );
+add_theme_support( 'post-thumbnails' );
+
+add_image_size( 'job-thumbnail', 120, 130 );
+function shop_register_nav_menu(){
+    register_nav_menus( array(
+        'primary_menu' => __( 'Primary Menu Name', 'mnk' ),
+        'footer_menu'  => __( 'Footer Menu', 'mnk' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'shop_register_nav_menu', 0 );
+
